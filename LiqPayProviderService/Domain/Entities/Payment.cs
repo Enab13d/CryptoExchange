b03bb2876f0f55edae31fe5fbd51e2f822273d77
Anton Maliutin <anton.maliutin@cyberpolice.gov.ua>
2025-11-03 14:56:40 +0200

    feature(entities/contracts)
    - extend FiatToCryptoResponseMessage (add create and update dates)
    - add userId into Payment entity

--- LiqPayProviderService/Domain/Entities/Payment.cs
+++ LiqPayProviderService/Domain/Entities/Payment.cs
@@ -9,6 +9,7 @@ public class Payment
     public ObjectId Id { get; set; }
     public Guid OrderId { get; init; }
     public Guid CorrelationId { get; set; }
+    public Guid UserId { get; set; }
     public decimal Amount { get; set; }
     public string Fiat { get; set; } = string.Empty;
     public string Crypto { get; set; } = string.Empty;
--- SharedContracts/FiatToCryptoResponseMessage.cs
+++ SharedContracts/FiatToCryptoResponseMessage.cs
@@ -10,5 +10,7 @@ namespace SharedContracts
     {
         public Guid CorrelationId { get; set; }
         public string Status { get; set; } = default!;
+        public string? CreateDate { get; set; }
+        public string? UpdateDate { get; set; }
     }
 }
